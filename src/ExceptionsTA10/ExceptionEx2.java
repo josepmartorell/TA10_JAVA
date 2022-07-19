0e84ddd771b9735b17d4bf8f4757eee4f2ab8e00
josepmartorell <martorelljosep@gmail.com>
2022-07-19 17:32:00 +0200

    added exercise two exception

--- src/ExceptionsTA10/ExceptionEx2.java
+++ src/ExceptionsTA10/ExceptionEx2.java
@@ -1,6 +1,28 @@
 package ExceptionsTA10;
 
 public class ExceptionEx2 extends Exception {
-	
+	private String code = "Unknown";
+    private String message;
+    
+	public ExceptionEx2 () {
+		super();
+	}
+
+    public ExceptionEx2 (String code) {
+    	super();
+        this.code = code;
+    }
+
+    public String getMessage() {
+    	switch (code) {
+        case "1":
+            this.message = "This is an object Exception";
+            break;
+        default:
+            this.message = "Unknown error";
+            break;
+    	}
+        return this.message;
+    }
 
 }
